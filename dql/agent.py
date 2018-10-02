93bb0bd3588358c8e28e4f73776ee98a8f9ca7d1
jsirol <johannes.sirola@outlook.com>
2018-10-02 19:21:14 +0300

    small refactor

--- dql/agent.py
+++ dql/agent.py
@@ -21,9 +21,6 @@ TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
 class Agent:
     """
     Agent class utilizing Deep Q-Learning to interact with the environment.
--- dql/model.py
+++ dql/model.py
@@ -29,39 +29,3 @@ class DQN(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
-
-
-class DQN2(nn.Module):
-
-    def __init__(self, state_size, action_size, seed, fc1_units=512,
-                 fc2_units=512, fc3_units=256, fc4_units=128, fc5_units=64):
-        """
-        Initialize parameters and build model.
-        :param state_size: Dimension of the states
-        :param action_size: Dimension of the actions
-        :param seed: seed for RNG
-        :param fc1_units: First hidden layer node count
-        :param fc2_units: Second hidden layer node count
-        """
-        super(DQN, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, fc4_units)
-        self.fc5 = nn.Linear(fc4_units, fc5_units)
-        self.fc6 = nn.Linear(fc5_units, action_size)
-
-    def forward(self, state):
-        """
-        Forward function for the network.
-        :param state: input state
-        :return: Tensor output of the last hidden layer
-        """
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = F.relu(self.fc6(x))
-        return x
